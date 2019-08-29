8c45f052b4852740296ab53f0bcdbe5dc748dab2
khawareji1 <54390013+khawareji1@users.noreply.github.com>
2019-08-29 23:54:36 +0530

    about.php
    some chenges  About , added photos and description

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,20 +1,47 @@
 <div class="container"> 
     
-    <h1>StockHub </h1><h4 style="color:black;"><b>is a raw material finder and procurement website which aids future and present manufacturers create links with vendors all across the country. It is an online platform for the manufacturing industry.</b></h4>
+    <center><h1>StockHub </h1><h4 style="color:black;"><b>StockHub is a raw material finder and procurement website which aids future and present manufacturers create links with vendors all across the country. It is an online platform for the manufacturing industry.</b></h4></center>
 <br>
     
     
-    <h2>How does it work?</h2>
+    <center><h2>StockHub Saves Your </h2></center>
     <hr>
-    <h4 style="text-align:justify;">The people who want to find out the raw materials required to make the end-product can do so by just typing out the name of the product in the  <a href="<?php echo base_url();?>home">homepage</a>.<br><br> 
+    <div>
+    <div class ="col-sm-6 col-md-4 col-lg-3 display-flex">
+      <div><img src="http://[::1]/stock/assets/images/money.png">
+      </div>
+        <div class="caption">
+          <h3>Money</h3>
+            <p class = "flex-text">Connects Various Manufacturers and Vendors So That It Saves The Time For Getting Materials</p>
+        </div>
+    </div>
+    <div class ="col-sm-6 col-md-4 col-lg-3 display-flex">
+        <div><img src="http://[::1]/stock/assets/images/time.png">
+            <div class="caption">
+            <h3>Time</h3>
+                <p class ="flex-text">Spend On Integration Once. No Need To Pay More On Custom Devlopment</p>
+            </div>
+        </div>
+    </div>
+        
+    <div class ="col-sm-6 col-md-4 col-lg-3 display-flex">
+        <div><img src="http://[::1]/stock/assets/images/humanresource.png">
+            <div class="caption">
+            <h3>Human Resource</h3>
+                <p class="flex-text">Concentrate on What Matters For Your Business. We Take Care Of Platform Updates / API Maintenance</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!--<h4 style="text-align:justify;">The people who want to find out the raw materials required to make the end-product can do so by just typing out the name of the product in the  <a href="<?php echo base_url();?>home">homepage</a>.<br><br> 
 Stockhub will show them the various <a href="<?php echo base_url();?>materials">raw materials</a> that can be used and the advantages and disadvantages of using each of them. This will give them a clarity of the materials that will be required and once that is achieved, the second function of the website starts.<br><br>
 A tender can be created to procure the product. The notification will be broadcasted to every vendor who deals with these materials. All this will create a network of interactions and save tons of research
-</h4>
-<hr><br>
+</h4>-->
+<br><br><br>
 <h3 style="text-align:center;"><a class="text-info" data-toggle="collapse" data-target="#contact">Contact Us</a></h3>
 
 <div style="text-align:center;" id="contact" class="collapse">
-<h3>Email : <a href="mailto:stockhub.christ@gmail.com" target="_top">stockhub.christ@gmail.com</a></h3>
+<h3>Email : <a href="mailto:khawarey16@gmail.com" target="_top">khawarey16@gmail.com</a></h3>
 </div>
 
 </div>  
